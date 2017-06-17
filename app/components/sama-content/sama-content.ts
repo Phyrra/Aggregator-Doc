0ef9ff2332f0e2203fcbe6a0a04752588a8217c4
Cabadath <>
2017-06-17 13:55:15 +0200

    implemented scrolling to topic

--- app/components/sama-content/sama-content.ts
+++ app/components/sama-content/sama-content.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { DOCUMENT } from '@angular/platform-browser';
+import $ from 'jquery-ts';
 
 @Component({
 	selector: 'sama-content',
 	templateUrl: './content.html'
 })
 export class SamaContent {
-	topic: string;
-
-	constructor(private route: ActivatedRoute) {}
+	constructor(private route: ActivatedRoute, @Inject(DOCUMENT) private document: any) { }
 
 	ngOnInit() {
 		this.route.params
-			.forEach((params: Params) => {
-				this.topic = params['topic'];
+			.subscribe((params: Params) => {
+				let topic: string = params['topic'];
+				if (!topic) {
+					return;
+				}
+
+				let $element: any = $('#' + topic);
+				if ($element.length === 0) {
+					return;
+				}
+
+				var $parent: any = $element.closest('sama-content');
+				
+				$parent.animate({
+					scrollTop: $element.offset().top - $parent.offset().top + $parent.scrollTop()
+				}, 500);
 			});
 	}
 }
